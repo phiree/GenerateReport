aa5c0b531ffcb4bd49f179224a5530e60c7b8bea
phiree <phiree@gmail.com>
2014-07-09 18:22:18 +0800

    add total import quantity in selected period

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 __author__ = 'Administrator'
-
-
 #connection_string='driver={SQL Server};server=server-pc\sqlexpress;database=TSNET1001;uid=ntsmyanmar;pwd=12345678'
-connection_string='driver={SQL Server};server=.\sqlexpress;database=TSNET1013;trusted_connection=true;'
+connection_string='driver={SQL Server};server=.\sqlexpress;database=TSNET1015;trusted_connection=true;'
